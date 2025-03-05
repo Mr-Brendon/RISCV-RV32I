842221efddb39cd2d40c6249b58cf040d4ac76b8
Mr_Brendon <104786666+Mr-Brendon@users.noreply.github.com>
2025-03-05 14:03:19 +0100

    Rename RISCV_Core.vhd to RISCV_Core.vhd

--- design/RISCV_Core.vhd
+++ design/RISCV_Core.vhd
@@ -9,20 +9,20 @@
 --
 --Comments: allora inizamo con capire come fare questa cpu buttando giu un po di idee.
 --Prima di tutto cosa voglio nel core: macchina a stati, i blocco da connettere?
---Forse entrambi cioè voglio i blocchi da collegare però anche la CU nel core magari 
---separata con dei trattini ------ però che sia dentro
+--Forse entrambi cioÃ¨ voglio i blocchi da collegare perÃ² anche la CU nel core magari 
+--separata con dei trattini ------ perÃ² che sia dentro
 --cerco di fare dei blocchi abbastanza semplici o comunque non con troppi sottoblocchi
 --facendo abbastanza behavioural o comunque non troppi blocchi di sottoblocchi
 --anche con il structural
---prima di tutto scrivo tutto ciò che faccio:
---ho gia fatto l'ALU poi la includerò, non ora perchè non c'ho voglia e devo mettere anche lo shifter.
+--prima di tutto scrivo tutto ciÃ² che faccio:
+--ho gia fatto l'ALU poi la includerÃ², non ora perchÃ¨ non c'ho voglia e devo mettere anche lo shifter.
 --
 --iniziamo a fare il program counter ricordando che deve poi saper fare i salti quindi deve avere mux e cose...
 --si potrebbe fare i macroblocchi per gli stadi di pipeline che non sarebbe male,
 --si potrebbe iniziare con il core con dentro la cu che va negli structural che sono gli stadi delle pipeline,
 --NO FACCIO COSI': nel core metto solo lo structural che collega gli stadi delle pipeline e la cu sempre
 --come compont.
---per il PC è interessante che deve esserci un +4 che dipende dall'istruazione di salto da vedere
+--per il PC Ã¨ interessante che deve esserci un +4 che dipende dall'istruazione di salto da vedere
 --
 ----------------------------------------------------------------------------------
 
