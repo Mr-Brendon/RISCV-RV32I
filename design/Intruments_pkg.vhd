a53dfc05d0cf3b8aa81cfc2f7c57a11fcc7f8064
Brando_Sp <104786666+Mr-Brendon@users.noreply.github.com>
2025-08-31 09:21:56 +0200

    Update Intruments_pkg.vhd

--- design/Intruments_pkg.vhd
+++ design/Intruments_pkg.vhd
@@ -5,5 +5,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 package Instruments_pkg is
 --Constants:
 constant Nbit: integer := 32;
-constant binary4in32: std_logic_vector(31 downto 0) := "00000000000000000000000000000100";
+constant pip_f_d: integer:= 64;
+constant binary4in32: std_logic_vector(31 downto 0) := "00000000000000000000000000000100"; --0x04
+constant op_BRANCH: std_logic_vector(6 downto 0) := "1100011"; --OPCODE for conditional jump instructions
+constant op_JALR: std_logic_vector(6 downto 0) := "1100111"; --OPCODE for JALR instruction
+constant op_JAL: std_logic_vector(6 downto 0) := "1101111"; --OPCODE for JAL instruction
+constant fu_JALR: std_logic_vector(2 downto 0):= "000"; --instruction for JALR in funct3
 end Instruments_pkg;
